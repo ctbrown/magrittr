ec7a48db08587fcfdcaf0cc651c3546515348153
Christopher Brown <cbrown@decisionpatterns.com>
2015-09-02 08:53:12 -0700

    Update wrap_function.R to first introspect recursive objects

--- R/wrap_function.R
+++ R/wrap_function.R
@@ -16,11 +16,34 @@
 # @return a function of a single argument, named \code{.}.
 wrap_function <- function(body, pipe, env)
 {
- 
+  
   if (is_tee(pipe)) {
     body <- call("{", body, quote(.))
   } else if (is_dollar(pipe)) {
     body <- substitute(with(., b), list(b = body))
   } 
-  eval(call("function", as.pairlist(alist(.=)), body), env, env)
+  
+  # browser()
+  
+  # Introspective search
+  #   modify body so that magrittr introspects recursive objects. 
+  #   1. test if this is an eligible call
+  #   2. test if the name can be dereferenced.
+  if(                                                # Requirement 1  
+      identical(pipe, quote(`%>%`)) &&                    # apply to %>% only 
+      ( length(body) == 2    &&                           # appears here as name(.)
+        is.name( body[[1]] ) &&  
+        identical( body[[2]], quote(`.`)  ) 
+       ) # dereferencing
+   ) { 
+    nm = as.character( body[[1]] )
+    body = bquote({ 
+      if( is.recursive(.) && exists( .(nm), . ) )    # Requirement 2
+        .[[ .(nm) ]] else
+        .(body)                                      # AST: body.[[2]][[4]]
+    })
+  } 
+  
+  eval( call("function", as.pairlist(alist(.=)), body), env, env)
+  
 }
